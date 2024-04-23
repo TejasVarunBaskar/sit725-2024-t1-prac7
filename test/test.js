b12ff8d24244ae28b67820b4d08b43de6bcba785
TejasVarunBaskar <11tejas07@gmail.com>
2024-04-23 14:57:37 +1000

    git commit

---
+++ test/test.js
@@ -0,0 +1,45 @@
+// test.js
+const url = 'http://localhost:3011/api/cats';
+
+const cat = {
+    title: "Fluffy",
+    subTitle: "The Fluffy Cat",
+    path: "https://www.thesprucepets.com/thmb/Wy9Vno45XeFtos7omJ80qkZrtZc=/3760x0/filters:no_upscale():strip_icc()/GettyImages-174770333-0f52afc06a024c478fafb1280c1f491f.jpg",
+    description: "A fluffy cat enjoying its nap."
+};
+
+describe('test GET api', function(){
+    it('return statusCode of 200', function(done){
+        // Using dynamic import for chai
+        import("chai").then(chai => {
+            const { expect } = chai;
+            const request = require("request");
+
+            request(url, function (error, response, bodyString){
+                if (bodyString) {
+                    let bodyObj = JSON.parse(bodyString);
+                    expect(bodyObj.message).to.equal('Get all cats successful');
+                }
+                done();
+            });
+        }).catch(err => done(err));
+    });
+});
+
+describe('test POST api', function(){
+    it('post cat to DB', function(done){
+        // Using dynamic import for chai
+        import("chai").then(chai => {
+            const { expect } = chai;
+            const request = require("request");
+
+            request.post({url: url, form: cat}, function (error, response, bodyString){
+                if (bodyString) {
+                    let bodyObj = JSON.parse(bodyString);
+                    expect(bodyObj.message).to.equal('Get all cats successful');
+                }
+                done();
+            });
+        }).catch(err => done(err));
+    });
+});
